3a3bad59458a8e4ec317d62ba7d863c4cb581184
Jefté Medeiros <jefte.medeiros8@gmail.com>
2026-07-29 00:01:20 -0300

    fix: debug 4

---
+++ cataloggi-backend-2/Migrations/20260728000000_AddUpdatedAtToCategories.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cataloggi_backend_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUpdatedAtToCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Categories",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Categories");
+        }
+    }
+}
